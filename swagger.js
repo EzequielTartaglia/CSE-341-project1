20ee6c5ff1021fc400d997407f40266ef18e2a1f
Ezequiel M. Tartaglia <96756615+EzequielTartaglia@users.noreply.github.com>
2024-11-08 15:01:34 -0300

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -1,15 +1,18 @@
 const swaggerAutogen = require('swagger-autogen')();
 
+const isProduction = process.env.NODE_ENV === 'production';
+const host = isProduction ? 'cse-341-project1-gmay.onrender.com' : 'localhost:8080';
+
 const doc = {
   info: {
     title: 'Contacts API',
     description: 'API documentation for contact routes'
   },
-  host: 'localhost:8080',
-  schemes: ['http']
+  host: host,
+  schemes: isProduction ? ['https'] : ['http']
 };
 
 const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js']; 
+const endpointsFiles = ['./routes/index.js'];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
